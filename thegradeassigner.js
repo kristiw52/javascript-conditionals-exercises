e24b7d1cac56d7b8489d5623001b945fc78681ad
Christopher Baek <baekc@amazon.com>
2016-12-13 19:56:39 -0800

    Convert html worksheets to js to run in node

---
+++ thegradeassigner.js
@@ -0,0 +1,10 @@
+/*
+The Grade Assigner
+
+- Write a function named assignGrade that:
+  - takes 1 argument, a number score.
+  - returns a grade for the score, either "A", "B", "C", "D", or "F".
+- Call that function for a few different scores and log the result to make sure it works.
+*/
+
+// YOUR CODE GOES HERE
---
+++ theworldtranslator.js
@@ -0,0 +1,10 @@
+/*
+The World Translator
+
+- Write a function named helloWorld that:
+  - takes 1 argument, a language code (e.g. "es", "de", "en")
+  - returns "Hello, World" for the given language, for atleast 3 languages. It should default to returning English.
+- Call that function for each of the supported languages and log the result to make sure it works.
+*/
+
+// YOUR CODE GOES HERE
